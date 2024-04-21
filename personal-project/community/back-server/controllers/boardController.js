1c20cef1558907e48dec6dbacdc10fcce8204e77
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-04-22 04:56:12 +0900

    feat: 게시글 수정시 발생하는 오류에 따라 상태코드 반환하는 기능 구현

--- personal-project/community/back-server/controllers/boardController.js
+++ personal-project/community/back-server/controllers/boardController.js
@@ -121,7 +121,14 @@ exports.updateBoard = async (req, res, next) => {
         return res.status(200).json(response);
 
     } catch (error) {
+        // 유효하지 않은 요청인 경우 : 400
+        // 인증되지 않은 사용자 요청인 경우 : 401
+        // 권한이 없는 사용자 요청인 경우 : 403
 
+        if (error.message == 'board_not_exist') {
+            return res.status(404).json({ 'message': error.message });
+        }
+        return res.status(500).json({ 'message': error.message });
     }
     
 }
