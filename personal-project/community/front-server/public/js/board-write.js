ebab1e5f288f0106269e9125e63eabdd30d28b33
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-04-22 01:16:25 +0900

    feat: 게시글 등록 기능 구현

--- personal-project/community/front-server/public/js/board-write.js
+++ personal-project/community/front-server/public/js/board-write.js
@@ -17,6 +17,60 @@ function showFileName() {
     }
 }
 
+// 생성 날짜
+const getCreatedDate = () => {
+    const createdAt = new Date();
+
+    const year = createdAt.getFullYear();
+    const month = ('0' + (createdAt.getMonth() + 1)).slice(-2);
+    const day = ('0' + createdAt.getDate()).slice(-2);
+
+    const dateString = year + '-' + month  + '-' + day;
+
+    const hours = ('0' + createdAt.getHours()).slice(-2); 
+    const minutes = ('0' + createdAt.getMinutes()).slice(-2);
+    const seconds = ('0' + createdAt.getSeconds()).slice(-2); 
+
+    const timeString = hours + ':' + minutes  + ':' + seconds;
+
+    return dateString + ' ' + timeString;
+}
+
+// 게시글 데이터 서버 전송
+const submitBoardData = async (event) => {
+    event.preventDefault();
+    const COMMON_URL = 'http://localhost:8080';
+    
+    // 더미 프로필 사진
+    const dummyImageURL = 'https://i.namu.wiki/i/w11dbZZeomJI4bD3_KItw3vq7tgglcM1YQA_xHULxMsixPpY1S7KcB8WrEFhJNuSuejiiQkicGKMH12JvpUqBQ.webp';
+    const createdDate = getCreatedDate();
+    const updatedDate = getCreatedDate();
+
+    const boardFormData = {
+        'title': titleInput.value,
+        'content': contentInput.value,
+        'image_url': dummyImageURL,
+        'created_at': createdDate,
+        'updated_at': updatedDate
+    }
+
+    const option = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(boardFormData)
+    }
+
+    const res = await fetch(`${COMMON_URL}/boards`, {
+        ...option
+    });
+
+    setTimeout(() => {
+        location.replace('/boards');
+    }, 2000);
+}
+
 function activeSubmitButton() {
     let isTitleEmpty = (titleInput.value.length == 0);
     let isContentEmpty = (contentInput.value.length == 0);
@@ -41,3 +95,5 @@ contentInput.addEventListener('keyup', activeSubmitButton);
 
 imageInputButton.addEventListener('click', imageInput.click());
 imageInput.addEventListener('change', showFileName);
+
+submitButton.addEventListener('click', submitBoardData);
