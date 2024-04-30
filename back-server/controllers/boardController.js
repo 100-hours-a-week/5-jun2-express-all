030edfe5f63810871fcfa84da53c36db72c64508
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-04-30 13:18:38 +0900

    feat: 인증된 사용자 게시글 등록 기능 구현

--- back-server/controllers/boardController.js
+++ back-server/controllers/boardController.js
@@ -1,4 +1,5 @@
 const boardRepository = require('../models/boardRepository');
+const userRepository = require('../models/userRepository');
 
 /* 1) 공통 로직 */
 
@@ -44,46 +45,21 @@ const getNowDate = () => {
 /* 2) 게시판 로직 */
 
 // 게시글 등록
-exports.registerBoard = async (req, res, next) => {
-    try {
-        const { title, content, image_url } = req.body;
-        const created_at = getNowDate();
-        const updated_at = getNowDate();
-        const board = { title, content, image_url, created_at, updated_at };
-
-        validateRequest(board);
-
-        console.log(board);
-
-        const findBoard = await boardRepository.save(board);
-        const response = getResponseMessage('register_success', findBoard);
-
-        // console.log("=== 모든 게시글 조회 ===");
-        // console.log(boardRepository.findAll());
-        // console.log("=== 게시글 조회 끝 ===\n");
-        
-        return res.status(201).json(response);
-    } catch (error) {
-        if (error.message == 'invalid_request') {
-            return res.status(400).json({ message: 'invalid_request' });
-        }
-
-        // 인증되지 않은 사용자 요청인 경우 : 401
-        // 권한이 없는 사용자 요청인 경우 : 403
-        // 존재하지 않은 사용자 요청인 경우 : 404
-
-        return res.status(500).json({ message: error.messsage });
-    }
-}
-
 exports.registerBoardWithImage = (req, res, next) => {
     try {
+        if (!req.session.user) {
+            throw new Error('unauthorized user');
+        }
+
         const title = req.body.title;
         const content = req.body.content;
         const created_at = getNowDate();
         const updated_at = getNowDate();
         const image_url = req.file.path;
-        const board = { title, content, image_url, created_at, updated_at };
+        const writer_id = req.session.user.id;
+        const writer = userRepository.findById(writer_id);
+
+        const board = { writer, title, content, image_url, created_at, updated_at };
 
         validateRequest(board);
 
@@ -101,9 +77,6 @@ exports.registerBoardWithImage = (req, res, next) => {
 // 게시글 목록 조회
 exports.findAllBoards = async (req, res, next) => {
     try {
-        console.log("=== board list 세션 ===");
-        console.log(req.session);
-        console.log("=== 세션 끝 ===\n\n");
         const boards = boardRepository.findAll();
         const response = getResponseMessage('success', boards);
 
@@ -115,10 +88,7 @@ exports.findAllBoards = async (req, res, next) => {
 
 // 게시글 상세 조회
 exports.findByBoardId = async (req, res, next) => {
-    try {
-        console.log("=== board detail 세션 ===");
-        console.log(req.session);
-        console.log("=== 세션 끝 ===\n\n");   
+    try { 
         const boardId = req.params.boardId;
 
         const findBoard = boardRepository.findById(boardId);
