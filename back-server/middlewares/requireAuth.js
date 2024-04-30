8ad11110621d468fa82733355334c4666a537301
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-04-30 11:48:53 +0900

    feat: 인증 미들웨어 구현

---
+++ back-server/middlewares/requireAuth.js
@@ -0,0 +1,17 @@
+const userRepository = require('../models/userRepository');
+
+exports.requireAuth = (req, res, next) => {
+    const session = req.session;
+
+    console.log("=== request header ===");
+    console.log(session);
+    console.log(session.user);
+
+    // 인증되지 않은 유저
+    if (!session.user) {
+        return res.status(401).json({ message: '로그인이 필요한 서비스입니다.' });
+    }
+
+    return res.status(200).json({ message: 'sucess' });
+    
+}
